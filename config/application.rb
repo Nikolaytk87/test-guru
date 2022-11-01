22ef7654bff97d4a51026a314dd7fe6f50ea7dff
Nikolay <nikolaytk87@mail.ru>
2022-11-01 13:10:32 -0500

    fix: Change default  locale on  en

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module TestGuru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.time_zone = 'Europe/Moscow'
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
