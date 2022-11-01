396044160b4af4f391541255a207fcb02e850756
Nikolay <nikolaytk87@mail.ru>
2022-11-01 12:20:37 -0500

    feat: add gem i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'about/index'
+  get '/about', to: 'about#index'
+  get '/about/author', to: 'about#author'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
