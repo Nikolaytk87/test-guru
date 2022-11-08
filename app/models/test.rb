2b81cee3dbcb700c1a948c1fbf811fe4991f1b65
Nikolay <nikolaytk87@mail.ru>
2022-11-08 20:30:17 +0600

    formatted

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,8 @@
 class Test < ApplicationRecord
   def self.filter(title)
-    joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: })
-                                                                 .order(title: :desc)
-                                                                 .pluck(:title)
+    joins('JOIN categories ON tests.category_id = categories.id')
+      .where(categories: { title: })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
