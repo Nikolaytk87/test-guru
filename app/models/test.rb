b61fd76687b96079f17e4f0daf45cc078903713e
Nikolay <nikolaytk87@mail.ru>
2022-11-10 09:51:17 +0600

    add model TestResult

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  has_many :tests_users
-  has_many :users, through: :tests_users
+  has_many :test_result
+  has_many :users, through: :test_result
   has_many :questions
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :test_result
+  has_many :tests, through: :test_result
   has_many :made_tests, class_name: 'Test', foreign_key: :author_id
   def tests_by_level(level)
     tests.where(level:)
