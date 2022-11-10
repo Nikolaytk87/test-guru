be5b398c784aaffb364e793b317ba3b96ccb29a2
Nikolay <nikolaytk87@mail.ru>
2022-11-10 11:59:58 +0600

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  has_many :test_results
-  has_many :users, through: :test_results
-  has_many :questions
   belongs_to :category
   belongs_to :author, class_name: 'User'
+
+  has_many :test_results
+  has_many :users, through: :test_results, dependent: :destroy
+  has_many :questions, dependent: :destroy
 
   def self.filter(title)
     joins(:category).where(categories: { title: }).order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@
 
 class User < ApplicationRecord
   has_many :test_results
-  has_many :tests, through: :test_results
-  has_many :made_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :tests, through: :test_results, dependent: :destroy
+  has_many :made_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
   def tests_by_level(level)
     tests.where(level:)
