97c5a8d320bf54a64fc44645e6eec4796f667096
Nikolay <nikolaytk87@mail.ru>
2022-11-10 10:25:17 +0600

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,7 @@ class Test < ApplicationRecord
   has_many :users, through: :test_results
   has_many :questions
   belongs_to :category
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
 
   def self.filter(title)
     joins(:category).where(categories: { title: }).order(title: :desc).pluck(:title)
