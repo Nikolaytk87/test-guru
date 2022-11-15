0198aede7fdedf588bd127d8666bb7a0d7589af3
Nikolay <nikolaytk87@mail.ru>
2022-11-15 20:34:21 +0600

    Advanced tasks

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,18 @@
 module TestsHelper
+  def question_header(test, action)
+    headers = { create: "Create New #{test.title} Question", edit: "Edit #{test.title} Question" }
+    headers[action]
+  end
+
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
+
+  def number_of_questions(test)
+    test.questions.count
+  end
 end
