45951a856d3d8e9aca7705c5ed842c6821bf2afe
Nikolay <nikolaytk87@mail.ru>
2022-11-18 16:44:06 +0600

    Lesson_9

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,13 +1,30 @@
 class TestPassage < ApplicationRecord
+  PASSED_RATE = 85
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_current_question
+
+  def successfully_passed_test?
+    percentage_correct_answers >= PASSED_RATE
+  end
+
+  def number_of_questions
+    test.questions.count
+  end
+
+  def current_question_number
+    test.questions.index(current_question).next
+  end
+
+  def percentage_correct_answers
+    correct_questions * 100 / test.questions.count
+  end
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-    self.current_question = next_question
     save!
   end
 
@@ -17,14 +34,16 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_current_question
+    self.current_question = if new_record?
+                              test.questions.first
+                            else
+                              next_question
+                            end
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-      correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort if answer_ids
   end
 
   def correct_answers
