81617b1ef6cbefeeaaa12a167815213092ae590b
Nikolay <nikolaytk87@mail.ru>
2022-11-20 14:14:06 +0600

    remove cookies original_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
     if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to original_path || tests_path
-      delete_original_path
     else
       flash.now[:alert] = 'Are you s Guru? Verify your Email and Password please'
       render :new
@@ -25,10 +24,6 @@ class SessionsController < ApplicationController
   private
 
   def original_path
-    cookies[:original_path]
-  end
-
-  def delete_original_path
     cookies.delete(:original_path)
   end
 
