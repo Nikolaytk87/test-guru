f696732b8ac757c1932f7c6ec9e382117152ceda
Nikolay <nikolaytk87@mail.ru>
2022-11-21 22:50:13 +0600

    flash to create

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,15 +4,15 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    flash[:alert] = "Hello, #{current_user.first_name}!"
-    super
-  end
-
-  # POST /resource/sign_in
-  # def create
+  # def new
   #   super
   # end
+
+  # POST /resource/sign_in
+  def create
+    super
+    flash.keep[:notice] = "Hello, #{current_user.first_name}!"
+  end
 
   # DELETE /resource/sign_out
   # def destroy
