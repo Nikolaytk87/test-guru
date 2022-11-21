d923d333c3038667073d70a607fa545686be97f9
Nikolay <nikolaytk87@mail.ru>
2022-11-21 22:32:10 +0600

    Advanced and formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :current_user,
                 :logged_in?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
 
   private
 
