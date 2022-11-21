86ce8df64f7ba95f319e8e0a14e1a58f72779f55
Nikolay <nikolaytk87@mail.ru>
2022-11-21 23:23:37 +0600

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users,
+             path: :gurus,
+             path_names: { sign_in: :login, sign_out: :logout },
+             controllers: { sessions: 'users/sessions' }
   root 'tests#index'
 
   resources :tests, only: :index do
